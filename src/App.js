467b1b5f88b8a8a4eb52143e507df007ff2fe411
keartiyot <jk.workingspace@gmail.com>
2022-04-30 13:21:43 +0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'h8k-components';
 
 import Articles from './components/Articles';
 
-const title = "Sorting Articles";
+const title = 'Sorting Articles';
 
-function App({articles}) {
+function App({ articles }) {
+  const [newArticles, setNewArticles] = useState(articles);
 
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
-            </div>
-            <Articles articles={articles}/>
-        </div>
-    );
+  useEffect(() => {
+    const sortByUpvotes = [...newArticles];
+    sortByUpvotes.sort((a, b) => {
+      return b.upvotes > a.upvotes ? 1 : -1;
+    });
+    setNewArticles(sortByUpvotes);
+  }, []);
 
+  const upvotedHandler = () => {
+    const sortByUpvotes = [...newArticles];
+    sortByUpvotes.sort((a, b) => {
+      return b.upvotes > a.upvotes ? 1 : -1;
+    });
+    setNewArticles(sortByUpvotes);
+  };
+
+  const recentHandler = () => {
+    const sortByDate = [...newArticles];
+    sortByDate.sort((a, b) => {
+      let na = new Date(a.date);
+      let nb = new Date(b.date);
+      return na > nb ? -1 : 1;
+    });
+    setNewArticles(sortByDate);
+  };
+
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="layout-row align-items-center justify-content-center my-20 navigation">
+        <label className="form-hint mb-0 text-uppercase font-weight-light">
+          Sort By
+        </label>
+        <button
+          data-testid="most-upvoted-link"
+          className="small"
+          onClick={upvotedHandler}
+        >
+          Most Upvoted
+        </button>
+        <button
+          data-testid="most-recent-link"
+          className="small"
+          onClick={recentHandler}
+        >
+          Most Recent
+        </button>
+      </div>
+      <Articles articles={newArticles} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Articles() {
-
-    return (
-        <div className="card w-50 mx-auto">
-            <table>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
+function Articles({ articles }) {
+  console.log(articles);
+  return (
+    <div className="card w-50 mx-auto" >
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articles &&
+            articles.map((i, key) => {
+              return (
+                <tr data-testid="article" key={key}>
+                  <td data-testid="article-title">{i.title}</td>
+                  <td data-testid="article-upvotes">{i.upvotes}</td>
+                  <td data-testid="article-date">{i.date}</td>
                 </tr>
-                </thead>
-                <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    );
-
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Articles;
